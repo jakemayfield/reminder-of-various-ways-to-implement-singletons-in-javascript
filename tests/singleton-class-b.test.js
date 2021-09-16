5e695adb64be86ad7b9967c4c23feb6051d0bdd5
Jake Mayfield <jacobpmayfield@gmail.com>
2021-09-16 14:38:44 -0400

    Add missing ;

--- tests/singleton-class-b.test.js
+++ tests/singleton-class-b.test.js
@@ -4,5 +4,5 @@ test('SingletonClassB is a singleton class', () => {
     const instanceOne = new SingletonClassB('Person B');
     const instanceTwo = new SingletonClassB('Person C');
     expect(instanceOne === instanceTwo).toEqual(true);
-    expect(instanceTwo.getName()).toEqual('Person B')
+    expect(instanceTwo.getName()).toEqual('Person B');
 });
